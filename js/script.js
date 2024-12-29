ab983dc4204bb86a3864795f2a29a00ef29b214a
Md Fahim <mdfahimhossen629@gmail.com>
2024-12-29 12:44:50 +0600

    Practice purpose

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+let form = document.querySelector("#form");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let fb_btn = document.querySelector("#fb_btn");
+
+function myCall() {
+    let validate = document.querySelector("#validate");
+
+if (!email.value && !password.value) {
+        validate.textContent = "দূর মিয়া কিছু লেখেন নাই!";
+        validate.style.color = "red";
+        validate.style.display = "block";
+    }else if(!password.value){
+        validate.textContent = "দূর মিয়া Password কে দিবে!";
+        validate.style.color = "red";
+        validate.style.display = "block";
+    }else if(!email.value){
+        validate.textContent = "দূর মিয়া Email কে দিবে!";
+        validate.style.color = "red";
+        validate.style.display = "block";
+    }
+    else {
+        validate.innerHTML = `Wrong credentials <br> Invalid username or password`;
+        validate.style.display = "block";
+    }
+}
+
+fb_btn.addEventListener('click', () => {
+    myCall();
+});
